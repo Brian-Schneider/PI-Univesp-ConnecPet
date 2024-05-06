8dde21329490416e8d51b3069a731da07379a55d
Brian Schneider de Lima <b.schneider.de.lima@avanade.com>
2024-05-06 15:26:51 -0700

    Criação do Projeto ConnecPet, criação dos principais Pacotes e configuração de conexão com o Banco de Dados

---
+++ src/main/java/com/pi/connecpet/ConnecpetApplication.java
@@ -0,0 +1,13 @@
+package com.pi.connecpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnecpetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnecpetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pi/connecpet/ConnecpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.connecpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnecpetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
